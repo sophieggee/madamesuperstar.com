5550b02dcba6c898d913a727e578dfd4eebe3a95
sophie gee <sophieggee@icloud.com>
2023-05-16 19:53:06 -0600

    epic moment alert

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let title: HTMLHeadingElement;
+
+	onMount(() => {
+		const titleWidth = title.clientWidth;
+
+		const animationDuration = (titleWidth / 100) * 2; // Adjust the division factor as needed
+		title.style.animationDuration = `${animationDuration}s`;
+	});
+</script>
+
+<header>
+	<h1 bind:this={title}>
+		MADAME SUPERSTAR MADAME SUPERSTAR MADAME SUPERSTAR MADAME SUPERSTAR MADAME SUPERSTAR MADAME
+		SUPERSTAR MADAME SUPERSTAR MADAME SUPERSTAR MADAME SUPERSTAR MADAME SUPERSTAR MADAME SUPERSTAR
+		MADAME SUPERSTAR MADAME SUPERSTAR MADAME SUPERSTAR MADAME SUPERSTAR MADAME SUPERSTAR MADAME
+		SUPERSTAR MADAME SUPERSTAR MADAME SUPERSTAR MADAME SUPERSTAR MADAME SUPERSTAR MADAME SUPERSTAR
+		MADAME SUPERSTAR
+	</h1>
+</header>
+
+<svelte:head>
+	<link rel="preconnect" href="https://rsms.me/" />
+	<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+</svelte:head>
+
+<slot />
+
+<!-- make background light pink -->
+<style>
+	:global(body) {
+		background: rgb(251, 208, 239);
+		margin: 0;
+		padding: 0;
+		font-family: 'Inter var', 'Inter', sans-serif;
+	}
+	h1 {
+		color: rgb(251, 208, 239);
+		padding: 0;
+		margin: 0;
+		font-size: 2rem;
+		font-weight: 600;
+		/* apply marquee */
+		animation: marquee linear infinite;
+		white-space: nowrap;
+		/* overflow: hidden; */
+	}
+	header {
+		background: rgb(255, 47, 0);
+		padding: 0 1rem;
+		overflow: hidden;
+	}
+	@keyframes marquee {
+		0% {
+			transform: translateX(-100%);
+		}
+		100% {
+			transform: translateX(0%);
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import geraniumImg from '../assets/img/geranium.jpeg';
+	let isHovering = false;
+</script>
+
+<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+<article on:mouseenter={() => (isHovering = true)} on:mouseleave={() => (isHovering = false)}>
+	<img src={geraniumImg} alt="geranium" />
+	{#if isHovering}
+		<div class="overlay">
+			<h1>A CASE FOR WILD</h1>
+		</div>
+	{/if}
+</article>
+
+<style>
+	img {
+		width: 100%;
+	}
+	article {
+		position: relative;
+		margin: 1rem 0;
+	}
+	.overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: rgb(255, 47, 0);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: rgb(251, 208, 239);
+		font-size: 4rem;
+	}
+</style>
